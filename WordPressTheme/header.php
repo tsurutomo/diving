60aaadb819987f1d3bdd496d7c25aa95aa42011a
tomo <zhihuichuanqi25@gmail.com>
2024-10-27 16:13:58 +0900

    comit

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php if (is_404()) : ?>
       <meta http-equiv="refresh" content=" 3; url=<?php echo esc_url(home_url("")); ?>">
   <?php endif; ?>
