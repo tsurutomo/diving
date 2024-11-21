1bbea1b6da72f79755d72d5d3dd222481ed4bbd3
tomo <zhihuichuanqi25@gmail.com>
2024-11-21 15:29:37 +0900

    comit

--- WordPressTheme/single.php
+++ WordPressTheme/single.php
@@ -73,21 +73,35 @@
           <h2 class="side__title">人気記事</h2>
           <div class="side__content">
           <ul class="side__article article">
-            <li class="article__item">
-              <a href="<?php the_permalink(); ?>">
-                <figure class="article__img">
-                  <?php if (has_post_thumbnail()) : ?>
-                    <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>のアイキャッチ画像">
-                  <?php else: ?>
-                    <img src="<?php echo get_theme_file_uri(); ?>/assets/images/noimage.jpg" alt="<?php the_title(); ?>">
-                  <?php endif; ?>
-                </figure>
-                <div class="article__body">
-                  <time class="article__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m-d'); ?></time>
-                  <p class="article__title"><?php the_title(); ?></p>
-                </div>
-              </a>
-            </li>
+          <?php
+              $popular_post_items = array(
+                'posts_per_page' => 3,
+                'meta_key'       => 'post_views_count',
+                'orderby'        => 'meta_value_num',
+              );
+              $popular_posts = new WP_Query($popular_post_items);
+
+              if ($popular_posts->have_posts()) :
+                while ($popular_posts->have_posts()) : $popular_posts->the_post(); ?>
+                  <li class="article__item">
+                    <a href="<?php the_permalink(); ?>">
+                      <figure class="article__img">
+                        <?php if (has_post_thumbnail()) : ?>
+                          <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>のアイキャッチ画像">
+                        <?php else: ?>
+                          <img src="<?php echo get_theme_file_uri(); ?>/assets/images/noimage.jpg" alt="<?php the_title(); ?>">
+                        <?php endif; ?>
+                      </figure>
+                      <div class="article__body">
+                        <time class="article__time" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m-d'); ?></time>
+                        <p class="article__title"><?php the_title(); ?></p>
+                      </div>
+                    </a>
+                  </li>
+                <?php endwhile;
+                wp_reset_postdata();
+              endif;
+            ?>
           </ul>
           </div>
         </div>
@@ -109,7 +123,7 @@
                   <?php if(get_the_post_thumbnail()) : ?>
                     <img src="<?php the_post_thumbnail_url(); ?>" width="602" height="402" alt="<?php the_title(); ?>のアイキャッチ画像">
                   <?php else: ?>
-                    <img src="<?php echo get_theme_file_uri('/assets/images/noimage.jpg'); ?>" width="602" height="402" alt="noimage">
+                    <img src="<?php echo get_theme_file_uri(); ?>/assets/images/noimage.jpg" width="602" height="402" alt="noimage">
                   <?php endif; ?>
                 </figure>
                 <div class="review__body">
@@ -155,7 +169,7 @@
                     <?php if(get_the_post_thumbnail()) : ?>
                       <img src="<?php the_post_thumbnail_url(); ?>" width="602" height="402" alt="<?php the_title(); ?>のアイキャッチ画像">
                     <?php else: ?>
-                      <img src="<?php echo get_theme_file_uri('/assets/images/noimage.jpg'); ?>" width="602" height="402" alt="noimage">
+                      <img src="<?php echo get_theme_file_uri(); ?>/assets/images/noimage.jpg" width="602" height="402" alt="noimage">
                     <?php endif; ?>
                   </figure>
                   <div class="campaign__card-body campaign__card-body--pd">
@@ -164,18 +178,16 @@
                   <div class="campaign__card-text-wrap campaign__card-text-wrap--pb">
                     <p class="campaign__card-text">全部コミコミ(お一人様)</p>
                     <div class="campaign__card-price-wrap">
-                      <p class="campaign__card-list-price">
-                        <?php $list_price = get_field('campaign__list-price');
-                          if($list_price){
-                            echo '￥' . number_format($list_price);
-                          } ?>
-                      </p>
-                      <p class="campaign__card-price campaign__card-price--lower">
-                        <?php $price = get_field('campaign__price');
-                          if($price){
-                            echo '￥' . number_format($price);
-                          } ?>
-                      </p>
+                      <?php if (($list_price = get_field('campaign__list-price'))) : ?>
+                        <p class="campaign__card-list-price">
+                          ¥<?= number_format($list_price); ?>
+                        </p>
+                      <?php endif; ?>
+                      <?php if (($price = get_field('campaign__price'))) : ?>
+                        <p class="campaign__card-price campaign__card-price--lower">
+                          ￥<?= number_format($price); ?>
+                        </p>
+                      <?php endif; ?>
                     </div>
                   </div>
                 </a>
@@ -195,29 +207,32 @@
           <h2 class="side__title">アーカイブ</h2>
           <div class="side__content">
           <ul class="side__archive archive">
-                    <?php
-                    // 最も古い年を取得
-                    $start_year = get_oldest_year();
-                    $current_year = date('Y');
+          <?php
+              $start_year = get_oldest_year();
+              $current_year = date('Y');
 
-                    // 各年をループ
-                    for ($year = $current_year; $year >= $start_year; $year--) {
-                        $year_count = get_year_archives_num($year); // 年の投稿数を取得
-                        if ($year_count > 0) { // 投稿がある年のみ表示
-                            echo '<li class="archive__item">';
-                            echo '<p class="archive__year" data-open-acd="archive-acd-' . $year . '">' . $year . '年</p>';
-                            // 各月をループ
-                            for ($month = 1; $month <= 12; $month++) {
-                                $month_count = get_month_archives_num($year, $month); // 月の投稿数を取得
-                                if ($month_count > 0) { // 投稿がある月のみ表示
-                                    echo '<p class="archive__month"><a href="' . get_month_link($year, $month) . '">' . $month . '月</a></p>';
-                                }
-                            }
-                            echo '</li>';
-                        }
-                    }
-                    ?>
-                  </ul>
+              for ($year = $current_year; $year >= $start_year; $year--) {
+                  $year_count = get_year_archives_num($year);
+                  if ($year_count > 0) {
+                      ?>
+                      <li class="archive__item">
+                          <p class="archive__year" data-open-acd="archive-acd-<?php echo $year; ?>"><?php echo $year; ?>年</p>
+                          <?php
+                          for ($month = 1; $month <= 12; $month++) {
+                              $month_count = get_month_archives_num($year, $month);
+                              if ($month_count > 0) {
+                                  ?>
+                                  <p class="archive__month"><a href="<?php echo get_month_link($year, $month); ?>"><?php echo $month; ?>月</a></p>
+                                  <?php
+                              }
+                          }
+                          ?>
+                      </li>
+                    <?php
+                  }
+              }
+            ?>
+            </ul>
           </div>
         </div>
       </div>
